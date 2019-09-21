432ab9f442d4fa992185f1b98fe2f990c5fd3492
Lawrence Ifeanyi <lawrence@sure.group>
2019-09-21 10:01:09 +0100

    Init commit

---
+++ src/emails/account.js
@@ -0,0 +1,23 @@
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'ifeanyilawrence.eze@gmail.com',
+        subject: 'We are thrilled to have you',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    });
+}
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'ifeanyilawrence.eze@gmail.com',
+        subject: 'Sorry to see you go',
+        text: `Goodbye, ${name}, I hope to see you back sometime soon.`
+    });
+}
+
+module.exports = { sendWelcomeEmail, sendCancelationEmail };
