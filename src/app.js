03903d090211822c054eda9b46b7971cc8cf0459
Lawrence Ifeanyi <ifeanyilawrence.eze@gmail.com>
2019-11-21 14:26:32 +0100

    Check and create public folder if it doesn't exist

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 require('./db/mongoose');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 const placeRouter = require('./routers/place');
 
 const app = express();
+
+var publicDirCheck = '../public';
+var uploadDirCheck = '../public/uploads';
+
+if (!fs.existsSync(publicDirCheck)){
+    fs.mkdirSync(publicDirCheck);
+}
+
+if (!fs.existsSync(uploadDirCheck)){
+    fs.mkdirSync(uploadDirCheck);
+}
+
 
 const publicDir = path.join(__dirname, '../public');
 
